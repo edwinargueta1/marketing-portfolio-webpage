abc8dd23f80dcfcd97ab0dcfeb58cb82a495dde7
Edwin Argueta <46907039+edwinargueta1@users.noreply.github.com>
2023-11-07 16:11:32 -0800

    Centered Nav Bar and Multi Page
    Change the nav bar location and look to be in the center of the page. Installed React Router to be able to change the pages when you click on a link on the nav bar.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import IconLinkedin from './Assets/svgIcons';
@@ -8,51 +9,25 @@ function App() {
     <>
       <body>
         <div id='headContent'>
-          <ul id="navBar">
-            <li id="profileName"><h1>Vanessa Martin Del Campo</h1></li>
-            <li id="home" className='navButton'><a href="https://www.google.com">Home</a></li>
-            <li id="contact" className='navButton'><a href="google.com">Contact</a></li>
-            <li id="info" className='navButton'><a href='#info'>Info</a></li>
-          </ul>
-
-
           <CircleProfilePhoto />
-          <h2 id='introQuote'>Boost Your Success with Data-Driven Marketing Strategies</h2>
-          {/** Another Example: Turning Ideas into Impact*/}
+          <div id='profileTitle'>
+          <h1 id='username'>Vanessa Martin Del Campo</h1>
+          <h3 id='title'>PR and Marketing</h3>
+          </div>
+
+          <NavBar/>
+
         </div>
 
         <div id='bodyContent'>
-          <div className='container'>
-            <div id='summarySection'>
-              <div id='summaryTitle'>
-                <h3>Hello there - Vanessa here</h3>
-              </div>
-              <div id='summary'><p>I'm an online marketing manager, specialized in social media and content marketing. I make data-based decisions for the best performance but I'm also not afraind to let my creativity shine.</p>
-                <p>With a pasion for all things marketing. I have honed my skills on creating and executing effective marketing strategies across a variety of channels.</p>
-                <p>With a keen eye for detail and deep underdtanding of consumer behavior. I appreciate every project with unique perspective, tailoring their approach to each client's specific needs.</p>
-                <button>Let's Chat</button>
-              </div>
-            </div>
-
-            <div id='pointSection'>
-              <InfoCards title="SEO" text="Optimize your pages and sites so that they show up on the top of Google every single time." />
-              <InfoCards title="CRO" text="Getting alot of traffic is great. But you know what is better? When they all convert." />
-              <InfoCards title="Brand Strategy" text="Establish a tone of voice, sort your visual branding and start making a marketing plan." />
-            </div>
-
-            <div id='projectSection'>
-              <h2>Sneek Peek of my recent projects</h2>
-              <div id='projects'>
-                <ProjectCards title='Project 1' text="Description of Project on thing and stuff that are important" />
-                <ProjectCards title='Beta Project' text="The second description for project 2 and its glory" />
-                <ProjectCards title='Project Tres' text="The third installment of vanessa glory and acomplishment" />
-              </div>
-            </div>
-
-            <div id='contactSection'>
-              <SimpleContactForm />
-            </div>
-          </div>
+          <Router>
+            <Routes>
+              <Route path='/' element={<HomePage/>} />
+              <Route path='/projects' element={<ProjectPage/>}/>
+              <Route path='/contact' element={<SimpleContactForm/>}/>
+              <Route path='*' element={<ErrorPage/>}/>
+            </Routes>
+          </Router>
         </div>
         <footer>
           <FooterSection name="Vanessa Martin Del Campo" location="Los Angeles, CA" email="martinvanessx3@gmail.com" />
@@ -60,6 +35,75 @@ function App() {
       </body>
     </>
   );
+}
+
+function ProjectPage(){
+  return(
+    <>
+      <h2>Project Something</h2>
+    </>
+  )
+}
+
+function HomePage() {
+  return (
+    <>
+      <div className='container'>
+        <div id='summarySection'>
+          <div id='summaryTitle'>
+            <h3>Hello there - Vanessa here</h3>
+          </div>
+          <div id='summary'><p>I'm an online marketing manager, specialized in social media and content marketing. I make data-based decisions for the best performance but I'm also not afraid to let my creativity shine.</p>
+            <p>With a pasion for all things marketing. I have honed my skills on creating and executing effective marketing strategies across a variety of channels.</p>
+            <p>With a keen eye for detail and deep underdtanding of consumer behavior. I appreciate every project with unique perspective, tailoring their approach to each client's specific needs.</p>
+            <button>Let's Chat</button>
+          </div>
+        </div>
+
+        <div id='pointSection'>
+          <InfoCards title="SEO" text="Optimize your pages and sites so that they show up on the top of Google every single time." />
+          <InfoCards title="CRO" text="Getting alot of traffic is great. But you know what is better? When they all convert." />
+          <InfoCards title="Brand Strategy" text="Establish a tone of voice, sort your visual branding and start making a marketing plan." />
+        </div>
+
+        <div id='projectSection'>
+          <h2>Sneek Peek of my recent projects</h2>
+          <div id='projects'>
+            <ProjectCards title='Project 1' text="Description of Project on thing and stuff that are important" />
+            <ProjectCards title='Beta Project' text="The second description for project 2 and its glory" />
+            <ProjectCards title='Project Tres' text="The third installment of vanessa glory and acomplishment" />
+          </div>
+        </div>
+        
+      </div>
+    </>
+  )
+}
+
+function ErrorPage(){
+  return(
+    <>
+      <h2>
+        Error this Page does not exist!
+      </h2>
+    </>
+  )
+}
+
+function NavBar() {
+  return (
+    <>
+      <nav>
+        <ul id="navList">
+          <div id='navWrapper'>
+            <li id="home" className='navButton'><a href="./">Home</a></li>
+            <li id="contact" className='navButton'><a href="./contact">Contact</a></li>
+            <li id="info" className='navButton'><a href='./projects'>Info</a></li>
+          </div>
+        </ul>
+      </nav>
+    </>
+  )
 }
 
 function CircleProfilePhoto() {
@@ -141,7 +185,8 @@ function SimpleContactForm() {
       .then(response => response.json())
       .then(data => {
         setConsoleMesssage(data.message);
-        return(console.log(data))})
+        return (console.log(data))
+      })
       .catch(error => console.log(error));
 
 
@@ -150,39 +195,41 @@ function SimpleContactForm() {
 
   return (
     <>
-      <div id='contactDirections'>
-                <h2>Get in Touch!</h2>
-                <p id='contactMessage'>Please provide some information on your goals and I'll get back to you in 2 business days to schedule a call.</p>
-                <p id='formConsole'>{consoleMessage}</p>
-              </div>
-      <form id='contactForm' onSubmit={handleSubmit}>
-      
-        <label for='contactName'>Name</label>
-        <input
-          id='contactName'
-          type='name'
-          placeholder='You Name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></input>
-        <label for='contactEmail'>Email</label>
-        <input
-          id='contactEmail'
-          type='email'
-          placeholder='name@email.com'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <label for='contactMessage'>Messsage</label>
-        <textarea
-          id='contactMessage'
-          rows='10'
-          placeholder='Start typing your message here...'
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        <button type='submit'>Send Mail</button>
-      </form>
+      <div id='contactSection'>
+        <div id='contactDirections'>
+          <h2>Get in Touch!</h2>
+          <p id='contactMessage'>Please provide some information on your goals and I'll get back to you in 2 business days to schedule a call.</p>
+          <p id='formConsole'>{consoleMessage}</p>
+        </div>
+        <form id='contactForm' onSubmit={handleSubmit}>
+
+          <label for='contactName'>Name</label>
+          <input
+            id='contactName'
+            type='name'
+            placeholder='You Name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <label for='contactEmail'>Email</label>
+          <input
+            id='contactEmail'
+            type='email'
+            placeholder='name@email.com'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <label for='contactMessage'>Messsage</label>
+          <textarea
+            id='contactMessage'
+            rows='10'
+            placeholder='Start typing your message here...'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <button type='submit'>Send Mail</button>
+        </form>
+      </div>
     </>
   )
 }
